e7c3d0f31059c5e34e37a3d9cef06eb9ec67cc39
Gerd Reiss <gerd@reiss.pro>
2020-11-19 11:49:24 +0100

    fix + extend

--- microsoft_reactor/moving_beyond_hello_world/kvstore/src/main.rs
+++ microsoft_reactor/moving_beyond_hello_world/kvstore/src/main.rs
@@ -2,15 +2,16 @@ use std::collections::HashMap;
 use std::fs::OpenOptions;
 use std::io;
 use std::io::prelude::*;
-use std::io::Error;
+
 struct Database {
     inner: HashMap<String, String>,
 }
 
 impl Database {
-    fn new() -> Result<Database, Error> {
-        let contents: String = std::fs::read_to_string("kv.db").expect("Failed to read database");
-        let db: HashMap<String, String> = contents
+    fn new() -> Database {
+        let contents: String = std::fs::read_to_string("kv.db").unwrap_or_default();
+
+        let entries: HashMap<String, String> = contents
             .lines()
             .map(|line| line.split('\t').collect::<Vec<&str>>()) // do I have to collect here?
             .filter(|split| !split.is_empty())
@@ -20,8 +21,9 @@ impl Database {
                 hmap
             });
 
-        Ok(Database { inner: db })
+        Database { inner: entries }
     }
+
     fn write(&self, key: String, value: String) -> io::Result<()> {
         let mut file = OpenOptions::new()
             .write(true)
@@ -30,20 +32,34 @@ impl Database {
             .open("kv.db")?;
         writeln!(file, "{}\t{}", key, value)
     }
+
+    #[allow(dead_code)]
+    fn write_al(&self) -> io::Result<()> {
+        let mut file = OpenOptions::new()
+            .write(true)
+            .append(true)
+            .create(true)
+            .open("kv.db")?;
+        self.inner
+            .iter()
+            .map(|(key, value)| writeln!(file, "{}\t{}", key, value))
+            .find(|r| r.is_err())
+            .unwrap_or_else(|| Ok(()))
+    }
 }
 
 fn main() {
     let mut args = std::env::args().skip(1);
     let key = args.next();
     let value = args.next();
+
     let database = Database::new();
 
-    match (database, key, value) {
-        (Ok(db), Some(k), Some(v)) => match db::write(k, v) {
+    match (key, value) {
+        (Some(k), Some(v)) => match database.write(k.clone(), v.clone()) {
             Err(e) => eprintln!("Couldn't write to file: {}", e),
             Ok(_) => println!("{}: {} added to database", k, v),
         },
-        (Err(e), _, _) => eprintln!("Couldn't read database file: {}", e),
 
         _ => println!("Both key and value must be defined: kvstore <key> <value>"),
     }
