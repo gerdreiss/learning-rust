0387e7cd8ae1999dbd721aedc253638d93dd0e9b
Gerd Reiss <gerd@reiss.pro>
2020-12-05 14:37:09 +0100

    Hands-On Systems Programmingj in Rust: error handling

--- udemy/hands-on-systems-programming/pipeviewer/src/main.rs
+++ udemy/hands-on-systems-programming/pipeviewer/src/main.rs
@@ -1,22 +1,31 @@
 use std::env;
-use std::io::{self, Read, Write};
+use std::io::{self, ErrorKind, Read, Write};
 
 const CHUNK_SIZE: usize = 16 * 1024;
 
-fn main() {
+fn main() -> io::Result<()> {
     let verbose = env::var("PV_SILENT").unwrap_or_default().is_empty();
     let mut total_bytes = 0;
+    let mut buffer = [0; CHUNK_SIZE];
     loop {
-        let mut buffer = [0; CHUNK_SIZE];
         let num_read = match io::stdin().read(&mut buffer) {
             Ok(0) => break,
             Ok(x) => x,
             Err(_) => break,
         };
         total_bytes += num_read;
-        io::stdout().write_all(&buffer[..num_read]).unwrap();
+        if verbose {
+            eprint!("\r{}", total_bytes);
+        }
+        if let Err(e) = io::stdout().write_all(&buffer[..num_read]) {
+            if e.kind() == ErrorKind::BrokenPipe {
+                break;
+            }
+            return Err(e);
+            // eprintln!("Oh no, an error! {}", e.to_string());
+            // std::process::exit(1);
+        }
     }
-    if verbose {
-        eprintln!("total_bytes: {}", total_bytes);
-    }
+
+    Ok(())
 }
