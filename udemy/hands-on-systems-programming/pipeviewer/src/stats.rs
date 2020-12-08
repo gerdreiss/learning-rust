4d1360aa725cf707841f352236898df15d80f2a8
Gerd Reiss <gerd@reiss.pro>
2020-12-08 10:35:13 +0100

    Hands-On Systems Programmingj in Rust: use crossterm to format output

--- udemy/hands-on-systems-programming/pipeviewer/src/stats.rs
+++ udemy/hands-on-systems-programming/pipeviewer/src/stats.rs
@@ -1,5 +1,10 @@
 use crossbeam::channel::Receiver;
-use std::io::Result;
+use crossterm::{
+    cursor, execute,
+    style::{self, Color, PrintStyledContent},
+    terminal::{Clear, ClearType},
+};
+use std::io::{self, Result, Stderr, Write};
 use std::time::{Duration, Instant};
 
 struct Timer {
@@ -39,7 +44,7 @@ impl TimeOutput for u64 {
     fn as_time(&self) -> String {
         let (hours, left) = (*self / 3600, *self % 3600);
         let (minutes, seconds) = (self / 60, left % 60);
-        format!("{}:{:02}:{:02}", hours, minutes, seconds)
+        format!("Time: {}:{:02}:{:02}; ", hours, minutes, seconds)
     }
 }
 
@@ -56,6 +61,7 @@ pub fn stats_loop(verbose: bool, stats_rx: Receiver<usize>) -> Result<()> {
     let mut total_bytes = 0;
     let start = Instant::now();
     let mut timer = Timer::new();
+    let mut stderr = io::stderr();
     loop {
         let num_bytes = stats_rx.recv().unwrap();
         timer.update();
@@ -63,11 +69,11 @@ pub fn stats_loop(verbose: bool, stats_rx: Receiver<usize>) -> Result<()> {
         total_bytes += num_bytes;
         if verbose && timer.ready {
             timer.ready = false;
-            eprint!(
-                "\r{} bytes in {} seconds [{:.0} b/s]",
+            output_progress(
+                &mut stderr,
                 total_bytes,
                 start.elapsed().as_secs().as_time(),
-                rate_per_second
+                rate_per_second,
             );
         }
         if num_bytes == 0 {
@@ -78,4 +84,19 @@ pub fn stats_loop(verbose: bool, stats_rx: Receiver<usize>) -> Result<()> {
         eprintln!();
     }
     Ok(())
+}
+
+fn output_progress(stderr: &mut Stderr, bytes: usize, elapsed: String, rate: f64) {
+    let bytes = style::style(format!("Processed {} bytes; ", bytes)).with(Color::Red);
+    let elapsed = style::style(elapsed).with(Color::Green);
+    let rate = style::style(format!("Rate: [{:.0} b/s] ", rate)).with(Color::Blue);
+    let _ = execute!(
+        stderr,
+        cursor::MoveToColumn(0),
+        Clear(ClearType::CurrentLine),
+        PrintStyledContent(bytes),
+        PrintStyledContent(elapsed),
+        PrintStyledContent(rate)
+    );
+    let _ = stderr.flush();
 }
