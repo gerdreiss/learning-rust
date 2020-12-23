1395111f623b39de05685b3c70f99d4b1c9c7ecd
Gerd Reiss <gerd@reiss.pro>
2020-12-23 14:16:08 +0100

    Rust Programming Recipes: Simple Patterns

--- udemy/programming-recipes/d4_builder/src/card.rs
+++ udemy/programming-recipes/d4_builder/src/card.rs
@@ -6,7 +6,7 @@ pub enum Ability {
     Taunt,
 }
 
-#[derive(Debug, PartialEq, PartialOrd, Eq, Ord)]
+#[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone, Copy)]
 pub enum Trigger {
     BattleCry,
     Death,
@@ -26,7 +26,7 @@ pub struct Card {
 
 impl Default for Card {
     fn default() -> Self {
-        Card { 
+        Card {
             name: "".to_owned(),
             strength: 1,
             health: 1,
@@ -43,6 +43,27 @@ impl Card {
     }
 }
 
+pub trait Triggerable {
+    fn trigger(&self, t: Trigger) -> Option<String>;
+}
+
+impl Triggerable for Card {
+    fn trigger(&self, t: Trigger) -> Option<String> {
+        self.triggers.get(&t).map(|s| s.to_string())
+    }
+}
+
+pub struct TriggerWrap<A: Triggerable, B: Triggerable> {
+    a: B,
+    b: B,
+}
+
+impl<A: Triggerable, B: Triggerable> Triggerable for TriggerWrap<A, B> {
+    fn trigger(&self, t: Trigger) -> Option<String> {
+        self.a.trigger(t).or_else(|| self.b.trigger(t))
+    }
+}
+
 #[derive(Debug, PartialEq, Default)]
 pub struct CardBuilder {
     pub name: String,
@@ -56,8 +77,8 @@ pub struct CardBuilder {
 impl CardBuilder {
     pub fn new(name: String) -> Self {
         CardBuilder {
-            name,            
-            ..Default::default(); // this is so awesome!
+            name,
+            ..Default::default() // this is so awesome!
         }
     }
     pub fn strength(mut self, s: i32) -> Self {
@@ -89,8 +110,10 @@ mod test_builder {
             .strength(4)
             .trigger(Trigger::BattleCry, "Deal 2 Damage")
             .build();
+
         let mut triggers = BTreeMap::new();
         triggers.insert(Trigger::BattleCry, "Deal 2 Damage".to_string());
+
         let expected = Card {
             name: "General Blight".to_string(),
             strength: 4,
@@ -100,5 +123,18 @@ mod test_builder {
             abilities: Vec::new(),
         };
         assert_eq!(expected, given)
+    }
+    #[test]
+    pub fn test_trigger_wrap() {
+        let c1 = Card::builder("c1".to_string())
+            .trigger(Trigger::BattleCry, "Cry Battle".to_string())
+            .build();
+        let c2 = Card::builder("c2".to_string())
+            .trigger(Trigger::Death, "Say Aaaargh".to_string())
+            .build();
+
+        let wrap = TriggerWrap { a: c1, b: c2 };
+        assert_eq!(wrap.trigger(Trigger::BattleCry).unwrap(), "Cry Battle");
+        assert_eq!(wrap.trigger(Trigger::Death).unwrap(), "Say Aaaargh");
     }
 }
