4db97251f5367e9679ee9444d5a49114f943f262
Gerd Reiss <gerd@reiss.pro>
2024-05-03 12:47:28 +0200

    Building web APIs with Rust (advanced): setup database connection

--- udemy/building-web-apis-advanced/cr8s/src/main.rs
+++ udemy/building-web-apis-advanced/cr8s/src/main.rs
@@ -1,7 +1,23 @@
+use rocket_db_pools::{Connection, Database};
+
 mod models;
 mod repositories;
 mod schema;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Database)]
+#[database("cr8s")]
+struct DatabaseConnection(rocket_db_pools::diesel::PgPool);
+
+#[rocket::get("/rustaceans")]
+fn get_rustaceans(database: Connection<DatabaseConnection>) {
+    // TODO
+}
+
+#[rocket::main]
+async fn main() {
+    let _ = rocket::build()
+        .mount("/", rocket::routes![get_rustaceans])
+        .attach(DatabaseConnection::init())
+        .launch()
+        .await;
 }
